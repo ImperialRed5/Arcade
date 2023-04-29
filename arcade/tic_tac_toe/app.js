e56b0eb58664d80b23072110adc1c6c8220b0c1d
ImperialRed5 <rosay_21@icloud.com>
2023-04-29 14:15:26 -0400

    preliminary styling edits/page layout

--- arcade/tic_tac_toe/app.js
+++ arcade/tic_tac_toe/app.js
@@ -1,50 +1,11 @@
-//This code is copied directly from the arcade workshop. I am using it as a visual starting point for my tic tac toe game.
-const gameState = {
-    players: ['x', 'o'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ]
-  }
+const gameBoard = document.querySelector( '#game-board' );
+const gameBoardCells = document.querySelectorAll( '.cell' );
+const gameBoardCellsArray = Array.from( gameBoardCells );
+const gameBoardCellsArrayLength = gameBoardCellsArray.length;
 
-// state
-let state;
+  // function createGameBoard() {
+  //   for ( let i = 0; i < gameBoardCellsArrayLength; i++ ) {
+  //     gameBoardCellsArray[ i ].setAttribute( 'data-cell-index', i );
+  //   }
+  // }
 
-function buildInitialState() {
-
-}
-
-// render
-function renderState() {
-
-}
-
-// maybe a dozen or so helper functions for tiny pieces of the interface
-
-// listeners
-function onBoardClick() {
-  // update state, maybe with another dozen or so helper functions...
-
-  renderState() // show the user the new state
-}
-const board = document.getElementById('board');
-board.addEventListener('click', onBoardClick); // etc
-
-// add to above
-function tick() {
-    // this is an incremental change that happens to the state every time you update...
-  
-    renderState()
-  }
-  
-  setInterval(tick, 1000 / 30) // as close to 30 frames per second as possible
-  
-  // now you might have things like
-  document.addEventListener('keydown', function (event) {
-    // here you might read which key was pressed and update the state accordingly
-  })
-
-
-  //need to write code for tic tac toe game
-  
